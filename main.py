f8cb1ea7466a77b6c26bd52f6119f0d24f94700b
sangamithra-s <sangamithra231@gmail.com>
2026-06-27 17:00:12 +0530

    Adding simple_agent.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from langchain!")
+import os
+import requests
+from dotenv import load_dotenv
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
 
+load_dotenv('keys.env', override=True)
 
-if __name__ == "__main__":
-    main()
+print("KEY:", os.environ.get("OPENAI_API_KEY")) 
--- simple_agent.py
+++ simple_agent.py
@@ -1,6 +1,36 @@
 import requests
-
+import os
 from dotenv import load_dotenv
 
-from langchain.agents import create_agent
-from langchain.tools import tool
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+
+
+load_dotenv('keys.env', override=True)
+
+@tool
+def get_weather(city: str) -> str:
+    """Get the current weather for a given city"""
+    response = requests.get(f'https://wttr.in/{city}?format=j1')
+    data = response.json()
+    return f"The weather in {city} is {data['current_condition'][0]['weatherDesc'][0]['value']}"
+
+model = ChatOpenAI(model='gpt-4o-mini')  
+
+agent = create_react_agent(
+    model=model,
+    tools=[get_weather],
+    prompt
+    ='You are a helpful assistant that can provide weather information for any city who always cracks jokes and is humorous while remaining helpful.'
+)
+
+response = agent.invoke({
+    'messages': [{          # ✅ 'messages' not 'message'
+        'role': 'user',
+        'content': 'What is the weather in New York?'
+    }]
+})
+
+print(response)
+print(response['messages'][-1].content)  # ✅ 'messages' not 'message'
