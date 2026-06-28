81947c98fb1c351031e0c2573faba51cd13eeeb2
sangamithra-s <sangamithra231@gmail.com>
2026-06-28 17:06:03 +0530

    Adding streamingband conversation agent and updating standalone model to support streamingband conversation agent.

--- standalone_model.py
+++ standalone_model.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage,AIMessage,SystemMessage
 
 load_dotenv('keys.env', override=True)
 
@@ -9,8 +10,20 @@ model = init_chat_model(
     temperature=0.1
 )
 
-response = model.invoke('Hello, what is python?')
+# conversation = [
+#     SystemMessage(content="You are a helpful assistant for questions regarding programming"),
+#     HumanMessage(content="Hello, what is python?"),
+#     AIMessage(content="Python is a high-level, interpreted programming language known for its simplicity and readability. It supports multiple programming paradigms, including procedural, object-oriented, and functional programming. Python is widely used for web development, data analysis, artificial intelligence, scientific computing, and more.")
+#     ,HumanMessage(content="What is the difference between python and java?")
+# ]
 
-print(response)
-print(response.content)
+# response = model.invoke(conversation)
+
+#Streaming response
+for chunk in model.stream('Hello, what is python?'):
+    print(chunk.text, end='', flush=True)
+
+
+# print(response)
+# print(response.content)
 
